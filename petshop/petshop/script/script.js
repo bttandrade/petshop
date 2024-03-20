3b53b11a37c9c678090f8b16a1fa50936ff16aff
Breno Andrade <brebritto@gmail.com>
2024-03-19 22:36:55 -0300

    melhorias no design

--- petshop/petshop/script/script.js
+++
@@ -1,7 +0,0 @@
-const btns = document.getElementsByClassName('btn-buy');
-
-for (let i = 0; i < btns.length; i++) {
-    btns[i].addEventListener('click', () => {
-        location.href = '../pages/shopping-cart.html';
-    });   
-}
