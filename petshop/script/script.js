33daf53209744d4178577490a1607f2570fb3b56
Breno Andrade <brebritto@gmail.com>
2024-04-04 20:33:24 -0300

    função do carrinho finalizada

--- petshop/script/script.js
+++ petshop/script/script.js
@@ -7,31 +7,42 @@
 // }
 
 function addToCart(event) {
-    const product = event.target.parentNode;
-    const productName = product.querySelector('h4').innerText;
-    const productImage = product.querySelector('img').src;
-    const productPrice = product.querySelector('h5').innerText;
-  
-    // Cria um objeto para representar o produto
-    const productData = {
+  const product = event.target.parentNode;
+  const productName = product.querySelector('h4').innerText;
+  const productImage = product.querySelector('img').src;
+  const productPrice = product.querySelector('h5').innerHTML;
+  const productQuant = 1;
+
+  // Cria um objeto para representar o produto
+  const productData = {
       name: productName,
       image: productImage,
-      price: productPrice
-    };
-  
-    // Obtém o carrinho atual do armazenamento local ou cria um novo array vazio
-    let cart = JSON.parse(localStorage.getItem('cart')) || [];
-  
-    // Adiciona o produto ao carrinho
-    cart.push(productData);
-  
-    // Salva o carrinho atualizado de volta no armazenamento local
-    localStorage.setItem('cart', JSON.stringify(cart));
-}
-  
-const buyButtons = document.querySelectorAll('.btn-buy');
-  
-buyButtons.forEach(button => {
-    button.addEventListener('click', addToCart);
-});
+      price: productPrice,
+      quant: productQuant,
+  };
 
+  // Obtém o carrinho atual do armazenamento local ou cria um novo array vazio
+  let cart = JSON.parse(localStorage.getItem('cart')) || [];
+
+  // Verifica se o produto já está no carrinho
+  const existingProductIndex = cart.findIndex(item => item.name === productData.name);
+
+  if (existingProductIndex !== -1) {
+      // Se o produto já estiver no carrinho, aumenta sua quantidade em 1
+      cart[existingProductIndex].quant++;
+  } else {
+      // Se o produto não estiver no carrinho, adiciona-o ao carrinho
+      cart.push(productData);
+  }
+
+  // Salva o carrinho atualizado de volta no armazenamento local
+  localStorage.setItem('cart', JSON.stringify(cart));
+
+  window.location.href = './shopping-cart.html';
+}
+
+const buyButtons = document.querySelectorAll('.btn-buy');
+
+buyButtons.forEach(button => {
+  button.addEventListener('click', addToCart);
+});
