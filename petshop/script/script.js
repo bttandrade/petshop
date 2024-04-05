4de25e0f52c3013dea500160860bcff748e9f8bb
Breno Andrade <brebritto@gmail.com>
2024-04-04 22:59:57 -0300

    total adicionado, noticias adicionadas e comentarios melhorados

--- petshop/script/script.js
+++ petshop/script/script.js
@@ -1,10 +1,13 @@
-//const btns = document.getElementsByClassName('btn-buy');
+const continueBuy = document.getElementById('continue-buy');
+const finish = document.getElementById('finish');
 
-// for (let i = 0; i < btns.length; i++) {
-//     btns[i].addEventListener('click', () => {
-//         location.href = '../pages/shopping-cart.html';
-//     });   
-// }
+continueBuy.addEventListener('click', () => {
+  location.href='products.html';
+});
+
+finish.addEventListener('click', () => {
+  alert('função ainda não adicionada');
+});
 
 function addToCart(event) {
   const product = event.target.parentNode;
@@ -13,29 +16,23 @@ function addToCart(event) {
   const productPrice = product.querySelector('h5').innerHTML;
   const productQuant = 1;
 
-  // Cria um objeto para representar o produto
   const productData = {
-      name: productName,
-      image: productImage,
-      price: productPrice,
-      quant: productQuant,
+    name: productName,
+    image: productImage,
+    price: productPrice,
+    quant: productQuant,
   };
 
-  // Obtém o carrinho atual do armazenamento local ou cria um novo array vazio
   let cart = JSON.parse(localStorage.getItem('cart')) || [];
 
-  // Verifica se o produto já está no carrinho
   const existingProductIndex = cart.findIndex(item => item.name === productData.name);
 
   if (existingProductIndex !== -1) {
-      // Se o produto já estiver no carrinho, aumenta sua quantidade em 1
       cart[existingProductIndex].quant++;
   } else {
-      // Se o produto não estiver no carrinho, adiciona-o ao carrinho
       cart.push(productData);
   }
 
-  // Salva o carrinho atualizado de volta no armazenamento local
   localStorage.setItem('cart', JSON.stringify(cart));
 
   window.location.href = './shopping-cart.html';
